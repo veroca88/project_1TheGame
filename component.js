c8f934ee36729687ae1e716285621bcf0af5f942
Veronica <veronica1506@gmail.com>
2019-12-09 14:20:10 -0500

    Player movements and background map done

--- component.js
+++ component.js
@@ -1,12 +1,51 @@
 class Component {
-    constructor(game, positionX, positionY, tileW, tileH) {
+    constructor(game, x, y, w, h) {
         this.game = game;
-        this.x = positionX;
-        this.y = positionY;
-        this.w = tileW;
-        this.h = tileH;
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
         this.img = new Image()
     }
+    drawComponent(imgSource) {
+        let daCtx = this.game.ctx;
+        this.img.src = imgSource;
+        // this.img.addEventListener("load", () => {
+            daCtx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        // });
+      }
+    move() {
+        document.onkeydown = event => {
+            //   console.log("event: ", event.keyCode);
+            const key = event.keyCode;
+            const possibleKeystrokes = [37, 65, 38, 87, 39, 83, 40, 68];
+            if (this.game.eachTile == 1) {
 
-    
+                if (possibleKeystrokes.includes(key)) {
+                    event.preventDefault();
+                    switch (key) {
+                        case 37:
+                        case 65:
+                            if (this.x >= 114) this.x -= 57;
+                            break;
+                        case 38:
+                        case 87:
+                            if (this.y >= 0) this.y -= 57;
+                            break;
+                        case 39:
+                        case 83:
+                            if (this.x <= 700 - this.width) this.x += 20;
+                            break;
+                        case 40:
+                        case 68:
+                            if (this.y <= 700 - this.height) this.y += 20;
+                            break;
+                    }
+                }
+            }
+            else {
+                console.log(`=============================== Wall`)
+            }
+        };
+    }
 }
--- game.js
+++ game.js
@@ -4,30 +4,35 @@ class Game {
         this.canvas = document.getElementById("game");
         this.ctx = this.canvas.getContext("2d");
         this.grass = new Image();
-        this.sand = new Image();
+        this.rockPath = new Image();
         this.wall = new Image();
+        this.blackHole = new Image();
+
+        this.brainPlayer = new Component(this, 70, 70, 65, 65);
+        this.key = new Component(this, 760, 760, 65, 65);
         this.cols = 16;
         this.row = 16;
-        this.tileW = 50;
-        this.tileH = 50;
+        this.tileW = 56.25;
+        this.tileH = 56.25;
         this.gameMap = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2, 1, 0],
-            [0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 0],
+            [0, 1, 1, 2, 2, 2, 2, 2, 0, 0, 1, 1, 1, 2, 1, 0],
+            [0, 1, 1, 2, 2, 2, 2, 2, 0, 0, 2, 1, 2, 2, 1, 0],
             [0, 1, 2, 2, 1, 1, 1, 1, 1, 2, 2, 1, 2, 2, 1, 0],
             [0, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 1, 0],
             [0, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 1, 0],
             [0, 1, 2, 1, 1, 1, 1, 2, 1, 2, 2, 2, 1, 2, 1, 0],
             [0, 1, 2, 1, 2, 2, 1, 2, 1, 2, 2, 1, 1, 1, 1, 0],
-            [0, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 2, 1, 1, 0],
-            [0, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1, 2, 1, 1, 0],
-            [0, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 1, 1, 0],
-            [0, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 2, 1, 1, 0],
-            [0, 1, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 1, 1, 0],
+            [0, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 2, 2, 1, 0],
+            [0, 0, 0, 1, 1, 2, 1, 1, 1, 2, 2, 1, 2, 2, 1, 0],
+            [0, 0, 0, 1, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 1, 0],
+            [0, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 2, 2, 1, 0],
+            [0, 1, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 1, 0],
             [0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0],
             [0, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ]
+        ],
+        this.eachTile;
     }
 
     init() {
@@ -38,13 +43,17 @@ class Game {
     start() {
         this.drawBackground()
         this.grass.src = "./images/grass.png";
-        this.sand.src = "./images/sand.png";
+        this.rockPath.src = "./images/rockPath.png";
         this.wall.src = "./images/wall.png";
+        this.blackHole.src = "./images/blackHole.png";
+        console.log(`====================================`)
         const interId = setInterval(() => {
-        // this.clear()
-        this.drawBackground()
-        // this.brain.move()
-        }, 1000)
+            // this.clear()
+            this.drawBackground()
+            this.brainPlayer.move()
+            this.brainPlayer.drawComponent("./images/brain.png");
+            this.key.drawComponent("./images/goldKey.png");
+        }, 500)
     }
 
     //   this.ctx.drawImage(this.wall, this.tileW*y/15, this.tileW*x/17, this.tileW/15, this.tileH/17);
@@ -54,30 +63,32 @@ class Game {
     drawBackground() {
         for (let x = 0; x < this.gameMap.length; x++) {
             for (let y = 0; y < this.gameMap[x].length; y++) {
-                
+                this.eachTile = this.gameMap[x][y]
+
                 // console.log(`===========${this.gameMap[y][x]}===============`)
 
-                switch (this.gameMap[x][y]) {
+                switch (this.eachTile) {
                     case 0:
-                        this.ctx.drawImage(this.wall, this.tileW * x, this.tileH * y, this.tileW, this.tileH);
+                        this.ctx.drawImage(this.grass, this.tileW * x, this.tileH * y, this.tileW, this.tileH);
                         // console.log(`case 0 ${this.wall.src}=================`)
                         break;
                     case 1:
-                        this.ctx.drawImage(this.sand, this.tileW * x, this.tileH * y, this.tileW, this.tileH);
+                        this.ctx.drawImage(this.rockPath, this.tileW * x, this.tileH * y, this.tileW, this.tileH);
                         // console.log(`case 1 ${this.wall}=================`)
                         break;
                     case 2:
-                        this.ctx.drawImage(this.grass, this.tileW * x, this.tileH * y, this.tileW, this.tileH);
+                        this.ctx.drawImage(this.wall, this.tileW * x, this.tileH * y, this.tileW, this.tileH);
                         // console.log(`case 2 ${this.wall}=================`)
                         break;
                     default:
                         this.ctx.drawImage(this.blackHole, this.tileW * x, this.tileH * y, this.tileW, this.tileH);
-                        // console.log(`case 3 ${this.wall}=================`)
+                    // console.log(`case 3 ${this.wall}=================`)
                 }
 
             }
         }
     }
+
 
 }
 
