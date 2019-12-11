c95158992cd0b6b194a34e263ef2694cc9fb657e
Veronica <veronica1506@gmail.com>
2019-12-10 19:54:12 -0500

    question feature on keyDown event

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ class Game {
         this.canvas = document.getElementById("game");
         this.ctx = this.canvas.getContext("2d");
         this.grass = new Image();
-        this.rockPath = new Image();
+        this.sand = new Image();
         this.wall = new Image();
         this.blackHole = new Image();
 
@@ -16,44 +16,49 @@ class Game {
         this.tileH = 56.25;
         this.gameMap = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+            [0, 1, 1, 2, 2, 2, 2, 2, 1, 4, 1, 1, 1, 1, 1, 0],
             [0, 1, 1, 2, 2, 1, 1, 1, 1, 0, 0, 1, 2, 2, 1, 0],
-            [0, 1, 2, 2, 1, 1, 2, 2, 1, 0, 0, 1, 2, 1, 1, 0],
-            [0, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 0, 0],
+            [0, 1, 2, 2, 1, 1, 2, 2, 1, 0, 0, 1, 2, 1, 4, 0],
+            [0, 1, 1, 4, 1, 2, 2, 2, 1, 1, 1, 4, 1, 1, 0, 0],
             [0, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 0, 0, 0],
-            [0, 1, 2, 1, 1, 1, 1, 2, 1, 2, 2, 2, 1, 0, 0, 0],
-            [0, 1, 2, 1, 2, 2, 1, 2, 1, 2, 2, 1, 1, 1, 1, 0],
-            [0, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 2, 2, 1, 0],
-            [0, 0, 0, 1, 2, 1, 1, 1, 1, 2, 2, 1, 2, 2, 1, 0],
+            [0, 1, 2, 1, 1, 4, 1, 2, 1, 2, 2, 2, 1, 0, 0, 0],
+            [0, 1, 2, 1, 2, 2, 1, 2, 4, 2, 2, 1, 1, 1, 1, 0],
+            [0, 1, 4, 1, 1, 2, 1, 2, 1, 2, 2, 1, 2, 2, 1, 0],
+            [0, 0, 0, 1, 2, 1, 1, 1, 1, 2, 2, 1, 2, 2, 4, 0],
             [0, 0, 0, 1, 2, 1, 0, 0, 1, 2, 2, 1, 2, 2, 1, 0],
-            [0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 2, 2, 1, 0],
-            [0, 1, 1, 2, 2, 1, 0, 0, 1, 2, 1, 2, 2, 2, 2, 0],
-            [0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0],
-            [0, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 0],
+            [0, 0, 1, 1, 1, 4, 0, 0, 1, 1, 4, 1, 2, 2, 1, 0],
+            [0, 1, 4, 2, 2, 1, 0, 0, 4, 2, 1, 2, 2, 2, 1, 0],
+            [0, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0],
+            [0, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ],
-        this.numberTile = undefined;
+            this.numberTile = undefined;
+
+        this.state = {
+            spiral: false,
+            questions: false
+        }
     }
 
     init() {
         this.start()
     }
-    
+
     start() {
         this.drawBackground()
         this.grass.src = "./images/grass.png";
-        this.rockPath.src = "./images/rockPath.png";
+        this.sand.src = "./images/sand.png";
         this.wall.src = "./images/wall.png";
         this.blackHole.src = "./images/blackHole.png";
         console.log(`====================================`)
         const interId = setInterval(() => {
             // this.clear()
             this.drawBackground()
-            // this.challenge(this.gameMap) //////////////////
             this.brainPlayer.move()
             this.brainPlayer.drawComponent("./images/brain.png");
             this.key.drawComponent("./images/goldKey.png");
-    
+            // this.brainPlayer.didCollide(this.blackHole)
+            // clearInterval(interId)
         }, 500)
     }
 
@@ -73,7 +78,7 @@ class Game {
                         // console.log(`case 0 ${this.wall.src}=================`)
                         break;
                     case 1:
-                        this.ctx.drawImage(this.rockPath, this.tileW * x, this.tileH * y, this.tileW, this.tileH);
+                        this.ctx.drawImage(this.sand, this.tileW * x, this.tileH * y, this.tileW, this.tileH);
                         // console.log(`case 1 ${this.wall}=================`)
                         break;
                     case 2:
@@ -88,5 +93,19 @@ class Game {
         }
     }
 
+    renderSpiral() {
+        let elementHtml = document.querySelector('#spiral')
+        let q =document.querySelector("#questions")
+        if (this.state.spiral && this.state.questions) {
+            elementHtml.style.visibility = "hidden";
+            q.style.visibility = "hidden";
+        }
+        else {
+            elementHtml.style.visibility = "visible";
+            q.style.visibility = "visible";
+        }
+    }
+
+    
 }
 
